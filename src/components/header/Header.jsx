3dc28c0fb2384830f5c4fc3df4d924dd2e0403f7
Atib Ashar <im.ashar96@gmail.com>
2025-05-06 05:16:26 +0530

    removed one header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -50,12 +50,7 @@ const Header = () => {
                         </a>
                     </li>
 
-                    <li className="nav__item">
-                        <a href="#portfolio" onClick={() =>
-                            setActiveNav("#portfolio")} className={ activeNav === "#portfolio" ? "nav__link active-link" : "nav__link"}>
-                            <i className="uil uil-scenery nav__icon"></i>Portfolio
-                        </a>
-                    </li>
+                   
 
                     <li className="nav__item">
                         <a href="#contact" onClick={() =>
