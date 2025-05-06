44020eccf1f55bfe81668814e209109665b3df65
Atib Ashar <im.ashar96@gmail.com>
2025-05-06 06:23:14 +0530

    changed icon style

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 const Info = () => {
   return (
         <div className="about__info grid">
-            <div className="about__box aout__icon">
+            <div className="about__box about__icon">
             <i class='bx bx-award'></i>
                 <h3 className="about__title">Experince</h3>
                 <span className="about__subtitle">1+ year working</span>
             </div>
 
-            <div className="about__box">
-            <i class='bx bx-briefcase aout__icon' ></i>
+            <div className="about__box about__icon">
+            <i class='bx bx-briefcase '></i>
                 <h3 className="about__title">Completed</h3>
                 <span className="about__subtitle">8+ Projects</span>
             </div>
 
-            <div className="about__box">
-            <i class='bx bx-support aout__icon' ></i>
+            <div className="about__box about__icon">
+            <i class='bx bx-support'></i>
                 <h3 className="about__title">Support</h3>
                 <span className="about__subtitle">Online 24/7</span>
             </div>  
